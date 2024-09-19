3c0b6354e997d891fe9858268fca35ec01833fb2
Jake Roeleven <jake.roeleven@skyciv.com>
2024-09-19 11:50:04 +1000

    Upload Minimal Template Repo

---
+++ calculate.js
@@ -0,0 +1,11 @@
+module.exports = function (input_json) {
+
+	let output = {
+		"report": REPORT,
+		"results": {
+		}
+	}
+
+	return output;
+
+}
---
+++ ui.js
@@ -0,0 +1,1 @@
+var input = SKYCIV_DESIGN.generateInput(true);
